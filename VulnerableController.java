bded414b9b3b598dbb71d130171c5e36368046e9
SumangalaBhat <51767187+SumangalaBhat@users.noreply.github.com>
2026-04-07 10:49:10 +0000

    Create VulnerableController.java

---
+++ VulnerableController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import org.apache.commons.collections4.map.LazyMap;
+
+@RestController
+public class VulnerableController {
+
+    // Intentionally vulnerable endpoint
+    @GetMapping("/vuln")
+    public String getVulnerable() {
+        LazyMap<String, String> map = LazyMap.lazyMap(Collections.emptyMap(), () -> "vulnerable");
+        map.put("test", null);
+        return "Vulnerability present!";
+    }
+}
